58b4f7c994e78188c05e06ed36eb4dcd5ec705b2
robertegelund <robert.egelund@outlook.com>
2023-10-16 16:26:34 +0200

    Implement reversed version of toplogical sorting

--- DirectedAcyclicGraph.java
+++ DirectedAcyclicGraph.java
@@ -13,10 +13,53 @@ public class DirectedAcyclicGraph<E extends Comparable<E>> extends DirectedWeigh
         graph.get(currentNode).put(neighbourNode, weight);
     }
 
+    Stack<E> dfsTopSort() {
+        Stack<E> stack = new Stack<>();
+        Set<E> visited = new HashSet<>();
+        for(E u : nodes.values()) {
+            if(!visited.contains(u)) {
+                dfsVisit(u, visited, stack);
+            }
+        }
+        return stack;
+    }
+
+    void dfsVisit(E u, Set<E> visited, Stack<E> stack) {
+        visited.add(u);
+        for(E v : graph.get(u).keySet()) {
+            if(!visited.contains(v)) {
+                dfsVisitReversed(v, visited, stack);
+            }            
+        }
+        stack.push(u);
+    }
+
+     Stack<E> dfsTopSortReversed() {
+        Stack<E> stack = new Stack<>();
+        Set<E> visited = new HashSet<>();
+        for(E u : nodes.values()) {
+            if(!visited.contains(u)) {
+                dfsVisitReversed(u, visited, stack);
+            }
+        }
+        return stack;
+    }
+
+    void dfsVisitReversed(E u, Set<E> visited, Stack<E> stack) {
+        visited.add(u);
+        stack.push(u);
+        for(E v : graph.get(u).keySet()) {
+            if(!visited.contains(v)) {
+                dfsVisitReversed(v, visited, stack);
+            }            
+        }
+    }
+
     Map<E, Integer> shortestPaths(E s) {
         Map<E, Integer> dist = new HashMap<>();
         dist.put(s, 0);
-        Stack<E> sorted = dfsTopSort();
+        Stack<E> sorted = dfsTopSortReversed();
+        System.out.println(sorted);
 
         for(E u : sorted) {
             for(E v : graph.get(u).keySet()) {
@@ -31,29 +74,24 @@ public class DirectedAcyclicGraph<E extends Comparable<E>> extends DirectedWeigh
 
         return dist;
     }
-    
 
-    //Depth first topological sorting
-    Stack<E> dfsTopSort() {
-        Stack<E> stack = new Stack<>();
-        Set<E> visited = new HashSet<>();
-        for(E u : nodes.values()) {
-            if(!visited.contains(u)) {
-                dfsVisit(u, visited, stack);
-            }
-        }
-        return stack;
+
+    public static void main(String[] args) {
+        DirectedAcyclicGraph<String> dag = new DirectedAcyclicGraph<>();
+        dag.add("a");
+        dag.add("b");
+        dag.add("c");
+        dag.add("d");
+        dag.addEdge("a", "b", 1);
+        dag.addEdge("b", "d", 3);
+        dag.addEdge("c", "d", 2);
+        dag.addEdge("a", "c", 4);
+
+        dag.printStructure();
+
+        Map<String, Integer> shortest = dag.shortestPaths("a");
+        System.out.println(shortest);
+
     }
 
-    //Depth first traversal adjusted for dfsTopSort
-    void dfsVisit(E u, Set<E> visited, Stack<E> stack) {
-        visited.add(u);
-        for(E v : graph.get(u).keySet()) {
-            if(!visited.contains(v)) {
-                dfsVisit(v, visited, stack);
-            }            
-        }
-        stack.push(u);
-        System.out.println(stack);
-    }
 }
