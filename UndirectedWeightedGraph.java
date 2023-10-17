73806ad9800b7ff9c594944769982fc33f6729a9
robertegelund <robert.egelund@outlook.com>
2023-10-17 13:56:51 +0200

    Make Relation as an inner class

--- UndirectedWeightedGraph.java
+++ UndirectedWeightedGraph.java
@@ -80,6 +80,23 @@ public class UndirectedWeightedGraph<E extends Comparable<E>> extends SimpleGrap
         return parents;
     }
 
+    class Relation<K, L> implements Comparable<Relation<K, L>> {
+        K parent; 
+        L child; 
+        int weight;
+        
+        Relation(K parent, L child, int weight) {
+            this.parent = parent;
+            this.child = child;
+            this.weight = weight;
+        }
+    
+        @Override
+        public int compareTo(Relation<K, L> relation) {
+            return this.weight - relation.weight;
+        }
+    }
+
     void updateWeights(Map<E, Integer> dist) {
         for(E u : graph.keySet()) {
             dist.putIfAbsent(u, 1000);
